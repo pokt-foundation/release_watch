1cf026f2b527fc1d121b2c04fee467bb4e8fc483
blockjoe <69400937+blockjoe@users.noreply.github.com>
2022-01-17 11:37:37 -0500

    Added start when running install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ cd "$root_dir"
 /usr/bin/env python3 -m venv ./venv
 ./venv/bin/pip install .
 
-mkdir -p ~/.config/systemd/user
 mkdir -p ./build
 cp ./config/release-watch.service ./build/release-watch.service
 sed -i "s|ExecStart=|ExecStart=${root_dir}/venv/bin/release_watch|g" ./build/release-watch.service
+
+mkdir -p ~/.config/systemd/user
 cp ./build/release-watch.service ~/.config/systemd/user
 systemctl --user enable release-watch.service
+systemctl --user start release-watch.service
+
+cd "$pdir"
